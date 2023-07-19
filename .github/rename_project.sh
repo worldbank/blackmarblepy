e41c800a7fe91a766639f600a1bbe7c41108c868
ramarty <ramarty@users.noreply.github.com>
2023-07-19 18:01:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ramarty"
+original_name="blackmarblepy"
+original_urlname="blackmarblepy"
+original_description="Awesome blackmarblepy created by ramarty"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv blackmarblepy $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
