33b4d2684512af38690279722a5786562baa88cc
Gabriel Stefanini Vicente <git@g4brielvs.me>
2025-06-06 16:10:12 -0400

    Improve product_id docs (#119)
    * Improve product_id docs
    
    * Improve requires-python

--- src/blackmarble/types.py
+++ src/blackmarble/types.py
@@ -4,7 +4,17 @@ from enum import Enum
 
 
 class Product(Enum):
-    """NASA Black Marble product suite (VNP46)"""
+    """
+    Representing of NASA's Black Marble VNP46 product suite.
+
+    Products:
+        - VNP46A1: Daily at-sensor top-of-atmosphere (TOA) nighttime radiance.
+        - VNP46A2: Daily moonlight-adjusted nighttime lights (NTL).
+        - VNP46A3: Monthly-aggregated, gap-filled nighttime light composites
+        - VNP46A4: Yearly-aggregated, gap-filled nighttime light composites.
+
+    For more details, see: https://blackmarble.gsfc.nasa.gov/#product
+    """
 
     VNP46A1 = "VNP46A1"
     VNP46A2 = "VNP46A2"
