4a223cb6bd76ad0ba1f2c25d6ca86b28f96477bb
Gabriel Stefanini Vicente <git@g4brielvs.me>
2025-11-11 11:55:09 -0500

    Update docuemntation

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,7 @@
 # re-generate this one.
 ###############################################################################
 apidoc_module_dir = "../src"
-author = (
-    "Gabriel Stefanini Vicente (git@g4brielvs.me), World Bank Development Data Group"
-)
+author = "World Bank Development Data Group"
 bibtex_bibfiles = ["bibliography.bib"]
 comments_config = {"hypothesis": False, "utterances": False}
 copyright = "2023"
